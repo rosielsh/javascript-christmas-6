0c2fd61e081dd246d1c36ec025e388713ebbaecf
suhwa <tnghk9611@naver.com>
2024-12-10 14:08:08 +0900

    test: 방문 날짜 범위 예외 테스트 작성

--- __tests__/validators/OrderValidator.test.js
+++ __tests__/validators/OrderValidator.test.js
@@ -1,7 +1,17 @@
+import OrderValidator from "../../src/validators/OrderValidator.js";
+
 describe("OrderValidator 클래스 테스트", () => {
   describe("식당 방문 날짜 유효성 검사", () => {
+    test("식당 방문 날짜는 숫자로 입력할 수 있다", () => {
+      const dates = [undefined, NaN, null, "string"];
+
+      dates.forEach((date) => {
+        expect(() => OrderValidator.checkDate(date)).toThrow("[ERROR]");
+      });
+    });
+
     test("식당 방문 날짜는 1~31 사이의 숫자로 입력할 수 있다", () => {
-      const dates = [undefined, NaN, null, "1", "a", 0, 32];
+      const dates = [0, 32, Infinity];
 
       dates.forEach((date) => {
         expect(() => OrderValidator.checkDate(date)).toThrow("[ERROR]");
