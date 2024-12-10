437c163f87fe6f6b0679c0160a85b51029e48ba3
suhwa <tnghk9611@naver.com>
2024-12-10 14:09:16 +0900

    feat: 방문 날짜 범위 예외 처리 구현

--- src/validators/OrderValidator.js
+++ src/validators/OrderValidator.js
@@ -4,10 +4,17 @@ import Validator from "./Validator.js";
 class OrderValidator {
   static checkDate(date) {
     this.#validateDateType(date);
+    this.#validateDateRange(date);
   }
 
   static #validateDateType(date) {
     if (Validator.isNotNumber(date)) {
+      generateError("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+  }
+
+  static #validateDateRange(date) {
+    if (date < 1 || date > 31) {
       generateError("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
   }
