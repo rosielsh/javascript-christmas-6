52f3a4b0f0edda37c2e47572f5a22c42858bbeb7
suhwa <tnghk9611@naver.com>
2024-12-10 17:48:03 +0900

    refactor: inputView, outputView 객체 형식으로 수정

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,33 +1,89 @@
 import { Console } from "@woowacourse/mission-utils";
 
-export default OutputView = {
+const OutputView = {
   printError(message) {
     Console.print(message);
-    printEmptyLine();
   },
 
   printStart() {
     Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n");
   },
 
-  printResult() {
-    Console.print("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n\n");
-    Console.print("<주문 메뉴>\n");
+  printResult(menus, eventInfo) {
+    Console.print("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n");
 
-    Console.print("<할인 전 총주문 금액>\n");
+    const logs = [];
+    logs.push("<주문 메뉴>");
 
-    Console.print("<증정 메뉴>\n");
+    for (let [menu, count] of menus) {
+      logs.push(`${menu} ${count}개`);
+    }
 
-    Console.print("<혜택 내역>\n");
+    logs.push("");
 
-    Console.print("<총혜택 금액>\n");
+    logs.push("<할인 전 총주문 금액>");
+    logs.push(`${eventInfo.totalPrice}원`);
 
-    Console.print("<할인 후 예상 결제 금액>\n");
+    logs.push("");
 
-    Console.print("<12월 이벤트 배지>\n");
-  },
+    logs.push("<증정 메뉴>");
+    logs.push(eventInfo.benefits["gift"] ? "샴페인 1개" : "없음");
 
-  printEmptyLine() {
-    Console.print("");
+    logs.push("");
+
+    logs.push("<혜택 내역>");
+
+    let benefitCount = 0;
+
+    for (let [key, price] of Object.entries(eventInfo.benefits)) {
+      switch (key) {
+        case "christmas": {
+          logs.push(`크리스마스 디데이 할인: -${price}원`);
+          benefitCount++;
+          break;
+        }
+        case "weekday": {
+          logs.push(`평일 할인: -${price}원`);
+          benefitCount++;
+          break;
+        }
+        case "weekend": {
+          logs.push(`주말 할인: -${price}원`);
+          benefitCount++;
+          break;
+        }
+        case "special": {
+          logs.push(`특별 할인: -${price}원`);
+          benefitCount++;
+          break;
+        }
+        case "gift": {
+          logs.push(`증정 이벤트: -${price}원`);
+          benefitCount++;
+          break;
+        }
+      }
+    }
+
+    if (!benefitCount) logs.push("없음");
+
+    logs.push("");
+
+    logs.push("<총혜택 금액>");
+    logs.push(`${eventInfo.totalBenefit}원`);
+
+    logs.push("");
+
+    logs.push("<할인 후 예상 결제 금액>");
+    logs.push(`${eventInfo.finalPay}원`);
+
+    logs.push("");
+
+    logs.push("<12월 이벤트 배지>");
+    logs.push(eventInfo["badge"] || "없음");
+
+    Console.print(logs.join("\n"));
   },
 };
+
+export default OutputView;
